d8558be6fdc16aa941f4f0d65ad49634f1976834
Rafael <robatistag@indracompany.com>
2021-04-25 21:41:23 -0300

    Criando método login em AccountController

--- src/main/java/com/academico/siac/controllers/AccountController.java
+++ src/main/java/com/academico/siac/controllers/AccountController.java
@@ -1,18 +1,22 @@
 package com.academico.siac.controllers;
 
 import com.academico.siac.models.Users;
-
 import com.academico.siac.services.UserService;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class AccountController {
+    @Autowired
+    private HttpServletRequest request;
 
     @Autowired
     private UserService userService;
@@ -20,6 +24,25 @@ public class AccountController {
     @GetMapping("/login")
     public String login() {
         return "account/login";
+    }
+
+    @PostMapping("/login")
+    public ModelAndView login(@Validated Users user, BindingResult result) {
+        ModelAndView mv = new ModelAndView();
+
+        Users usr = userService.encontrarPorEmail(user.getEmail());
+
+        boolean checkPassword = BCrypt.checkpw(user.getPassword(), usr.getPassword());
+        boolean checkSameEmail = user.getEmail().equals(usr.getEmail());
+
+        if (checkPassword && checkSameEmail) {
+            mv.setViewName("redirect:/students/list");
+        } else {
+            mv.addObject("user", user);
+            mv.setViewName("account/login");
+        }
+
+        return mv;
     }
 
     @GetMapping("/registration")
